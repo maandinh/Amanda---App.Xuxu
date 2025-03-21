d23543d49dbda78f6032a96ed878df2922892b05
Mandinha <161322821+maandinh@users.noreply.github.com>
2025-03-21 10:31:49 -0300

    Atividade 2

--- app.js
+++ app.js
@@ -2,60 +2,24 @@ import React, { Component } from "react";
 import { View, Text, Image, ScrollView, Button } from 'react-native';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: ''
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome) {
-    this.setState({
-      nome: nome
-    });
-  }
-
   render() {
     return (
       <ScrollView style={{ padding: 10 }}>
       
       <Topo imagem={{ uri: 'https://i.pinimg.com/736x/4d/c7/e7/4dc7e7134cf48216dbece029a792c4a2.jpg' }}></Topo>
-        <Salada 
-          imagem={{ uri: "https://i.pinimg.com/736x/1b/a2/b6/1ba2b6271f63c759ad6f38d5d9fc13e7.jpg" }}
-          titulo="Salada Waldorf"
-          receita={`
-            1/4 de xícara de chá de iogurte natural, 
-            1/4 de xícara de chá de maionese, 
-            suco de 1 limão, sal e pimenta-do-reino a gosto, 
-            1 cacho de uva sem semente cortadas ao meio, 
-            1 talo de salsão picado, 
-            2 maçãs verdes ou vermelhas em cubos, 
-            1/2 xícara de chá de nozes tostadas, 
-            folhas de alface a gosto.
-          `}
-        />
-        
-        <Salada      
-          imagem={{ uri: 'https://i.pinimg.com/736x/30/13/32/301332bbc7fb314466873f3d5b34e546.jpg' }}
-          titulo="Salada César"
-          receita={`
-            1 pé de alface romana, 
-            1/2 xícara de queijo parmesão ralado, 
-            1 xícara de croutons, 
-            Molho César a gosto, 
-            1 peito de frango grelhado e fatiado.
-          `}
-        /> 
 
-        <Button
-          title="Entrar"
-          onPress={() => this.entrar('Seja bem-vindo à aplicação')}
+        <SaladaCaesar 
+          imagem={{ uri: "https://i.pinimg.com/736x/23/f7/d6/23f7d6bd173fccf563760b1685485ce2.jpg" }}
+          titulo="Salada Caesar"
         />
-        <Text style={{ fontSize: 30, color: 'red', textAlign: 'center' }}>
-          {this.state.nome}
-        </Text>
+        <SaladaCaprese 
+          imagem={{ uri: "https://i.pinimg.com/736x/7b/35/ec/7b35ecc474bb29cb25a7625e7a631fbc.jpg"}}
+          titulo="Salada Caprese"
+        />
+        <SaladadeGrãos
+          imagem={{ uri: }}
+          />
+
       </ScrollView>
     );
   }
@@ -64,36 +28,56 @@ class App extends Component {
 class Topo extends Component {
   render() {
     return (
-      <View style={{ marginBottom: 20, padding: 5, borderRadius: 20, backgroundColor: '#f0f0f0'  }}>
+      <View style={{ marginBottom: 20, padding: 1, borderRadius: 20, backgroundColor: '#a6b985'  }}>
       
          <Image
           source={this.props.imagem}
-          style={{ width: '80%', height: 400, borderRadius: 20, marginTop: 40, overflow: 'hidden', transform: [{ rotate: '90deg' }]  }}
+          style={{ width: '100%', height: 400, borderRadius: 20, marginTop: 0, overflow: 'hidden', }}
         />   
-          <Text style={{ color: '#006400', fontSize: 40, marginBottom: 10, fontFamily: 'Emblema One' }}>
+          <Text style={{ color: '#006400', fontSize: 50, marginBottom: 10, fontFamily: 'Ink Free',  padding: '15px', fontWeight: 'bold' }}>
           Saladas
+        </Text>
+
+        <Text style={{ fontSize: 20, color: '#4f2621', fontFamily: 'Ink Free' ,padding: '15px', fontWeight: 'bold' }}>
+          Refrescantes, coloridas e cheias de sabor, as saladas são leves, crocantes e perfeitas para qualquer momento. Além de nutritivas, deixam a refeição mais equilibrada e deliciosa. Experimente com molhos naturais, queijos, castanhas e um toque de azeite!
         </Text> 
+
       </View>
     );
   }
 }
 
-class Salada extends Component {
+class SaladaCaesar extends Component {
   render() {
     return (
-      <View style={{ marginBottom: 20, padding: 10, borderRadius: 20 }}>
-        <Image
+      <View style={{ marginBottom: 20, padding: 1, borderRadius: 20, backgroundColor: '#ebd592', flexDirection: 'row', alignItems: 'center'  }}>
+      <Image
           source={this.props.imagem}
-          style={{ width: 300, height: 200, borderRadius: 20 }}
+          style={{ width: 150,  height: 150, borderRadius: 75,}}
         />
-        <Text style={{ fontWeight: 'bold', fontSize: 18, fontFamily: 'Emblema One' }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 40, fontFamily: 'Ink Free', padding: '15px' }}>
           {this.props.titulo}
         </Text>
-        <Text style={{ marginVertical: 2, fontSize: 15, fontFamily: 'Emblema One' }}>
-          {this.props.receita}
-        </Text>
+
       </View>
-    );
+    )
+  }
+}
+
+class SaladaCaprese extends Component {
+  render() {
+    return (
+      <View style={{ marginBottom: 20, padding: 1, borderRadius: 20, backgroundColor: '#ebd592', flexDirection: 'row', alignItems: 'center' }}>
+       <Text style={{ fontWeight: 'bold', fontSize: 40, fontFamily: 'Ink Free', padding: '15px'}}>
+      {this.props.titulo}
+    </Text>
+    <Image
+        source={this.props.imagem}
+        style={{ width: 150, height: 150, borderRadius: 75}}
+    />
+   
+      </View>
+    )
   }
 }
 
